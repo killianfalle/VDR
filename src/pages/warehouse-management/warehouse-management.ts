c2d754e66f9a0a8bd34976e82e69951649d171bd
Michael Ruther Batoon <michaelruther95@gmail.com>
2018-09-05 11:48:00 +0800

    - Added update warehouse functionalities

--- src/pages/warehouse-management/warehouse-management.ts
+++ src/pages/warehouse-management/warehouse-management.ts
@@ -16,7 +16,13 @@ import { DataProvider } from '../../providers/data-provider';
 })
 export class WarehouseManagementPage {
 	showAddForm: boolean = false;
-	form: any = {
+	showEditForm: boolean = false;
+	addForm: any = {
+		'name': '',
+		'address': '',
+	};
+	updateForm: any = {
+		'id': '',
 		'name': '',
 		'address': '',
 	};
@@ -41,8 +47,9 @@ export class WarehouseManagementPage {
   	}
 
   	setAddForm(showForm){
-  		for(var key in this.form){
-  			this.form[key] = '';
+  		this.clearApiValidators();
+  		for(var key in this.addForm){
+  			this.addForm[key] = '';
   		}
   		this.showAddForm = showForm;
   	}
@@ -55,10 +62,7 @@ export class WarehouseManagementPage {
 	    loader.present();
 	    this.clearApiValidators();
 
-  		this.provider.postData(this.form ,'save-new-warehouse').then((res: any) => {
-	      	console.log("RESPONSE IS:");
-	      	console.log(res);
-
+  		this.provider.postData(this.addForm ,'save-new-warehouse').then((res: any) => {
 	      	this.warehouseList.push(res.whInfo);
 	      	this.showAddForm = false;
 
@@ -79,6 +83,60 @@ export class WarehouseManagementPage {
 	      
 	    });
   	}
+
+  	selectToEdit(warehouseID){
+  		console.log("WAREHOUSE ID: "+warehouseID);
+  		this.clearApiValidators();
+  		for(var counter = 0; counter < this.warehouseList.length; counter++){
+  			if(this.warehouseList[counter]['id'] == warehouseID){
+  				this.updateForm['id'] = this.warehouseList[counter]['id'];
+  				this.updateForm['name'] = this.warehouseList[counter]['name'];
+  				this.updateForm['address'] = this.warehouseList[counter]['address'];
+  				this.showEditForm = true;
+  				break;
+  			}
+  		}
+  		console.log("UPDATE FORM:");
+  		console.log(this.updateForm);
+  	}
+
+  	submitEditForm(){
+  		const loader = this.loadingCtrl.create({
+	      	content: "Please wait...",
+	      	duration: 3000
+	    });
+	    loader.present();
+	    this.clearApiValidators();
+
+	    this.provider.postData(this.updateForm ,'update-warehouse').then((res: any) => {
+	    	for(var counter = 0; counter < this.warehouseList.length; counter++){
+	    		if(this.warehouseList[counter]['id'] == this.updateForm.id){
+	    			this.warehouseList[counter]['name'] = this.updateForm['name'];
+	    			this.warehouseList[counter]['address'] = this.updateForm['address'];
+	    			break;
+	    		}
+	    	}
+
+	      	this.showEditForm = false;
+
+			const alert = this.alertCtrl.create({
+				title: 'Warehouse updated!',
+				subTitle: 'Warehouse successfully updated...',
+				buttons: ['OK']
+			});
+			alert.present();
+
+	      	loader.dismiss();
+	    }).catch((error) => {
+	    	var errors = JSON.parse(error._body).error;
+	    	for(var key in errors){
+	    		this.apiValidators[key] = errors[key][0];
+	    	}
+	    	loader.dismiss();
+	      
+	    });
+  	}
+
 
   	clearApiValidators(){
   		for(var key in this.apiValidators){
